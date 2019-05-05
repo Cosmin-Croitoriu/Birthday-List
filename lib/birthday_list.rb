065df740700c3cafaa26b3c1481e964333732c52
Student <comsin.c.croitoriu@gmail.com>
2019-05-05 17:04:11 +0100

    added the birthday check test and method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,4 @@
+require 'date'
 class BirthdayList
   def initialize
     @list = []
@@ -9,9 +10,42 @@ class BirthdayList
   end
 
   def display_list
-   @list.each{|element|
-       @result += "name: #{element[:name]}, dob: #{element[:dob]}\n"
+    @list.each { |element|
+      @result += "name: #{element[:name]}, dob: #{element[:dob]}\n"
     }
     @result.strip
   end
+
+  def check_bday
+    birthday_people = ""
+    @list.each { |person|
+      dob = person[:dob]
+      if birthday_day?(dob) && birthday_month?(dob)
+        age = calculate_age(dob)
+        birthday_people +=
+        "It's #{person[:name]}'s birthday today! They are #{age} years old!\n"
+      end
+    }
+    birthday_people.strip
+  end
+
+private
+  def birthday_day?(dob)
+    Date.parse(dob).day == Date.today.day
+  end
+
+  def birthday_month?(dob)
+    Date.parse(dob).month == Date.today.month
+  end
+
+  def calculate_age(dob)
+    Date.today.year - Date.parse(dob).year
+  end
+
 end
+
+bdl = BirthdayList.new
+bdl.add("Ciprian", "01-01-2001")
+bdl.add("Rox", "05-05-2011")
+bdl.display_list
+bdl.check_bday
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,18 +1,11 @@
 require 'birthday_list'
 
 describe BirthdayList do
-  birthday_list = BirthdayList.new
-  it "adds the name and the birthday to the list" do
-    expect(birthday_list).to respond_to(:add).with(2).arguments
-  end
+
   describe '#add' do
     it "adds a name and a date into the list" do
-      birthday_list = BirthdayList.new
-      expect(birthday_list.add('Ciprian', '14/06/1984')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }]
+      expect(subject.add('Ciprian', '14/06/1984')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }]
     end
-
-
-
 
     it "adds 2 sets of name and date into the list" do
       subject.add('Ciprian', '14/06/1984')
@@ -25,6 +18,14 @@ describe BirthdayList do
       subject.add('Ciprian', '14/06/1984')
       subject.add('Roxana', '12/02/1989')
       expect(subject.display_list).to eq("name: Ciprian, dob: 14/06/1984\nname: Roxana, dob: 12/02/1989")
+    end
+  end
+
+  describe '#check_bday' do
+    it "displays who celebrates a bday today from your list" do
+      allow(Date).to receive(:today).and_return Date.new(2019, 1, 1)
+      subject.add('Mary Poppin', '01/01/2001')
+      expect(subject.check_bday).to eq("It's Mary Poppin's birthday today! They are 18 years old!")
     end
   end
 end
