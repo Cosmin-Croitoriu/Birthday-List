5e77814b968c452121af0e894852e67f13fe288a
Student <comsin.c.croitoriu@gmail.com>
2019-05-05 15:03:11 +0100

    display returns the list in a nice format

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,13 +1,17 @@
 class BirthdayList
   def initialize
     @list = []
+    @result = ''
   end
 
   def add(name, date)
     @list << { name: name, dob: date }
   end
 
-  def list
-    puts @list
+  def display_list
+   @list.each{|element|
+       @result += "name: #{element[:name]}, dob: #{element[:dob]}\n"
+    }
+    @result.strip
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -7,17 +7,24 @@ describe BirthdayList do
   end
   describe '#add' do
     it "adds a name and a date into the list" do
+      birthday_list = BirthdayList.new
       expect(birthday_list.add('Ciprian', '14/06/1984')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }]
     end
 
+
+
+
     it "adds 2 sets of name and date into the list" do
-      expect(birthday_list.add('Roxana', '12/02/1989')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }, { name: 'Roxana', dob: '12/02/1989' }]
+      subject.add('Ciprian', '14/06/1984')
+      expect(subject.add('Roxana', '12/02/1989')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }, { name: 'Roxana', dob: '12/02/1989' }]
     end
   end
 
-  describe '#list' do
-    it "lists the all the names in the list" do
-      expect(birthday_list.list).to eq(puts @list)
+  describe '#display_list' do
+    it "displays all the names in the list in a nice format" do
+      subject.add('Ciprian', '14/06/1984')
+      subject.add('Roxana', '12/02/1989')
+      expect(subject.display_list).to eq("name: Ciprian, dob: 14/06/1984\nname: Roxana, dob: 12/02/1989")
     end
   end
 end
