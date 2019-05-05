281e98133850867d6f20cc0bca6cc29b1311f261
Student <comsin.c.croitoriu@gmail.com>
2019-05-05 12:38:12 +0100

    first tests for 'add' and 'list' the names and birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,13 @@
+class BirthdayList
+  def initialize
+    @list = []
+  end
 
+  def add(name, date)
+    @list << { name: name, dob: date }
+  end
+
+  def list
+    puts @list
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,23 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  birthday_list = BirthdayList.new
+  it "adds the name and the birthday to the list" do
+    expect(birthday_list).to respond_to(:add).with(2).arguments
+  end
+  describe '#add' do
+    it "adds a name and a date into the list" do
+      expect(birthday_list.add('Ciprian', '14/06/1984')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }]
+    end
+
+    it "adds 2 sets of name and date into the list" do
+      expect(birthday_list.add('Roxana', '12/02/1989')).to eq [{ name: 'Ciprian', dob: '14/06/1984' }, { name: 'Roxana', dob: '12/02/1989' }]
+    end
+  end
+
+  describe '#list' do
+    it "lists the all the names in the list" do
+      expect(birthday_list.list).to eq(puts @list)
+    end
+  end
+end
